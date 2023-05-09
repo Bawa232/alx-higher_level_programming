a23bdcc89ef07df5a286fd721fa161a14614691a
James Bawa <jbawa032@gmail.com>
2023-05-09 13:53:45 -0700

    done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,14 +3,14 @@
 int check_cycle(listint_t *list)
 {
 	if(list == NULL)
-		return 0;
+		return (0);
 
 	listint_t *ptr = list;
 
-	while(ptr->next)
+	while(ptr->next != NULL)
 		ptr = ptr->next
-		if(ptr->next == list
-			return 0;
+		if(ptr->next == list)
+			return (0);
 	
-	return 0;
-)
+	return (0);
+}
