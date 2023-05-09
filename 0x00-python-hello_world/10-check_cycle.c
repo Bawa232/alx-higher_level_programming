0259bad621e1ae583f7b02a35d3faa53083835c1
James Bawa <jbawa032@gmail.com>
2023-05-09 14:37:33 -0700

    done

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,16 +1,25 @@
 #include "lists.h"
 
+/**
+ * check_cycle - check for circle in a linked list
+ *
+ * @list: Pointer to the head node
+ *
+ * Return: 0 for success else 1
+ */
+
 int check_cycle(listint_t *list)
 {
-	if(list == NULL)
+	listint_t *current;
+
+	if (list == NULL)
 		return (0);
-
-	listint_t *ptr = list;
-
-	while(ptr->next != NULL)
-		ptr = ptr->next
-		if(ptr->next == list)
-			return (0);
-	
+	current = list;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		if (current->next == list)
+			return (1);
+	}
 	return (0);
 }
