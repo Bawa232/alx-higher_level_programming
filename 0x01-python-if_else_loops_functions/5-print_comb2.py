500f1947f0fb866f4ef61667772afefa12de08b3
James Bawa <jbawa032@gmail.com>
2023-05-10 10:13:26 -0700

    padding

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print("{}".format(i))
+for i in range(0, 100):
+    if i / 10 == 9.9 and i % 10 == 9:
+        print(i)
     else:
-        print("{0:02}".format(i), end = ", ")
+        print("{:02d}, ".format(i), end="")
