77b8c13ce970ccbfee9f183a097123bf8d01e841
James Bawa <jbawa032@gmail.com>
2023-05-13 10:23:06 -0700

    we wiin

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@
 if __name__ == "__main__":
     import hidden_4
     items = dir(hidden_4)
+    items = sorted(items)
     for item in items:
         if (item[0] and item[1]) == "_":
             continue
