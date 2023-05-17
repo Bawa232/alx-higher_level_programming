f0ce31691b37c2321070678c9efac702420d04f7
James Bawa <jbawa032@gmail.com>
2023-05-17 13:19:52 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    power = [matrix[i][j] ** 2 for i in range(len(matrix)) for j in range(len(matrix))]
+    power = []
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            power.append(matrix[i][j] ** 2)
 
     return power
