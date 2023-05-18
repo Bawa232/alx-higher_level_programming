af6fb7811fb831c0ee82e60dfe261e2f257ee9ef
James Bawa <jbawa032@gmail.com>
2023-05-18 04:33:54 -0700

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    new_set1 = list(set_1)
+    new_set2 = list(set_2)
+    new = []
+
+    for i in range(len(new_set1)):
+        for j in range(len(new_set2)):
+            if new_set1[i] == new_set[2]:
+                new.append(new_set1[i])
+
+    return new
