8ccf2ce3bcc3fb7f6fa71bca190f019b6dc55ab5
James Bawa <jbawa032@gmail.com>
2023-05-18 07:56:26 -0700

    update dict

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    new_set1 = list(set_1)
-    new_set2 = list(set_2)
-    new = []
+    """Function that returns a set of common elements in two sets
 
-    for i in range(len(new_set1)):
-        for j in range(len(new_set2)):
-            if new_set1[i] == new_set[2]:
-                new.append(new_set1[i])
+    Args:
+        set_1 (set): first set
+        set_2 (set): second set
 
-    return set(new)
+    Returns:
+        set: intersection of set_1 and set_2
+    """
+    return set(set_1).intersection(set_2)
