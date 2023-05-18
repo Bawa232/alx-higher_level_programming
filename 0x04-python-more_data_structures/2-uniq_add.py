b22b7bd2efbb53ccb928f0dc2706b3703b0fecfe
James Bawa <jbawa032@gmail.com>
2023-05-17 20:56:30 -0700

    uniq add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    my_list = set(my_list)
-    sum = sum(my_list, 0)
+    my_set = set(my_list)
+    my_set = sum(my_set, 0)
 
-    return sum
+    return my_set
