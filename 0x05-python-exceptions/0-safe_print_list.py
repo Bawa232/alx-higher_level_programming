b9507807140107858bb562b37402ca3a90e23db5
James Bawa <jbawa032@gmail.com>
2023-05-29 08:38:03 -0700

    should work

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,5 @@ def safe_print_list(my_list=[], x=0):
             except IndexError:
                 print()
 
-        if count == x:
-            print()
-
+        print()
         return count
