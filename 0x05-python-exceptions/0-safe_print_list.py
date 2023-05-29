d787060240dc4e719f1418f772fe628dededc0c7
James Bawa <jbawa032@gmail.com>
2023-05-29 08:11:22 -0700

    fid

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,9 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end="")
                 count += 1
             except IndexError:
-                break
-        print()
+                print()
+
+        if count == x:
+            print()
+
         return count
