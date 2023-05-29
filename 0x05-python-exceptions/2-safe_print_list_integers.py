7e6a7b3a3431b3fca0683fc8ec2932537ae4b47c
James Bawa <jbawa032@gmail.com>
2023-05-29 11:29:35 -0700

    safe printing

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    if(my_list):
+
+        count = 0
+
+        for i in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            except ValueError:
+                pass
+            except IndexError as e:
+                print(e)
+        print()
+        return count
