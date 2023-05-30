985e09bb4b8bd48169b790cae43e74f9373b8a39
James Bawa <jbawa032@gmail.com>
2023-05-30 09:16:46 -0700

    killing this

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+A module that sets size of square as private field
+
+"""
+
+
+class Square:
+    """Square class
+
+    private instance field(s) : size
+    methods: __init__
+
+    """
+
+    def __init__(self, size=0):
+        """initializes the class
+
+        Args:
+            size (int, optional): size of square. Defaults to 0.
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+            Args: it takes no arguments
+
+            Return: It returns the size of the area
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """ Takes no arguments and returns the size of the area"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ setter method to set the value of the size"""
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """ prints the value of size """
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
