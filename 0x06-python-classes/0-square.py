3459a860aa0f4b342d69d713b59e806386fc64dd
James Bawa <jbawa032@gmail.com>
2023-05-30 05:53:02 -0700

    an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" An empty class named square """
+
 
 class Square:
     """
-    Args: 
+    Args:
         it takes no argument, just an empty class
 
     Atrributes: it has no atrributes
