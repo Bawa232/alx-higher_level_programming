e1772a252047592b7bcc4bf8d206d5a643e59049
James Bawa <jbawa032@gmail.com>
2023-05-30 06:09:54 -0700

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A class named square that inializes a private attribute"""
+
+
+class Square:
+    """
+        Args:
+            __size a private variable
+        Attribute:
+            __size: an integer with size of the square
+    """
+
+    def __init__(self, __size):
+        self.__size = __size
