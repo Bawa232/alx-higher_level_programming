ddf45ea41aa0817ccb186396a582ee47f3637b44
James Bawa <jbawa032@gmail.com>
2023-06-04 14:23:28 -0700

    all ok

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,8 +21,26 @@ def matrix_divided(matrix, div):
     """
     s = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        if not isinstance(row, list) for row in matrix:
-            raise TypeError(s)
+        for row in matrix:
+            if not isinstance(row, list):
+                raise TypeError(s)
 
-    if any(len(row) != len(matrix[0]) for row in matrix:
-        raise TypeError("Each row of the matrix must have the same size")
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_list = []
+
+    for rows in matrix:
+        round_list = []
+        for element in rows:
+            round_list += [round(element/div, 2)]
+        new_list.append(round_list)
+
+    return new_list
