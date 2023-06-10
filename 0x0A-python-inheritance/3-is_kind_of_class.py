9939cffa0e13f4badf8b8239431820568618a53e
James Bawa <jbawa032@gmail.com>
2023-06-10 14:38:40 -0700

    instance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@ is an instance of a class """
 
 
 def is_kind_of_class(obj, a_class):
-    """ returns true if the object is 
+    """ returns true if the object is
     an instance of the class
 
     Args
