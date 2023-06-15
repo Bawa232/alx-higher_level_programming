a437cd54f09fcdff0923d98547f185d2d582c5c2
James Bawa <jbawa032@gmail.com>
2023-06-15 10:58:31 -0700

    pyth to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+''' converting a ython onject to json '''
+
+
+def to_json_string(my_obj):
+    '''returns the json rep of an
+    objext string '''
+
+    return json.dumps(my_obj)
