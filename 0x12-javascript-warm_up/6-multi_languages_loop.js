682064334c2809ebf638d5019a55542cc79f1c07
James Bawa <jbawa032@gmail.com>
2023-08-18 12:07:37 -0700

    languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+const num = languages.length;
+
+for (let i = 0; i < num; i++) {
+  console.log(languages[i]);
+}
