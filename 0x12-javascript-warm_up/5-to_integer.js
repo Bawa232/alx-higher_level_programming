78f820370bf3b9c4dd8a5d6b31f1295f76fadb38
James Bawa <jbawa032@gmail.com>
2023-08-18 11:46:19 -0700

    please work

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+  console.log('Not a number');
+} else if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  const newNum = parseInt(process.argv[2]);
+  console.log(newNum);
+}
