33d214794ad2b5efb8c4b26a9810d29a0ec5ff26
James Bawa <jbawa032@gmail.com>
2023-08-19 04:11:48 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const length = process.argv.length;
+
+if (length === 2 || isNaN(process.argv[2])) {
+  console.log('NaN');
+} else {
+  let num = parseInt(process.argv[2]);
+
+  for (let i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  console.log(num);
+}
