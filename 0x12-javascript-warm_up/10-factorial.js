9bab4d0ff6ff2aee78d0da56cc84b92dfba4c0c3
James Bawa <jbawa032@gmail.com>
2023-08-19 04:21:31 -0700

    trying

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
 
-const length = process.argv.length;
-
-if (length === 2 || isNaN(process.argv[2])) {
-  console.log(1);
-} else {
-  let num = parseInt(process.argv[2]);
-
-  for (let i = num - 1; i > 0; i--) {
-    num *= i;
-  }
-  console.log(num);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
+
+console.log(factorial(Number(process.argv[2])));
