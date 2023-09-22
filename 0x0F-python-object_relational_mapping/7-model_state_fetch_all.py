5e13fbfbccc830f0b34cffb83d5999e2ed6a477c
Bawa232 <jbawa032@gmail.com>
2023-09-22 15:29:24 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,12 +13,14 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     # check command line arguments
     if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <mysql username> <mysql password> <database name>")
+        print(f"Usage: {sys.argv[0]}
+        <mysql username> <mysql password> <database name>")
         sys.exit(1)
 
     # connect to database using SQLAlchemy
     username, password, db_name = sys.argv[1:]
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(f'mysql+mysqldb://{username}:
+    {password}@localhost:3306/{db_name}')
     Base.metadata.create_all(engine)
     LocalSession = sessionmaker(bind=engine)
     db = LocalSession()
