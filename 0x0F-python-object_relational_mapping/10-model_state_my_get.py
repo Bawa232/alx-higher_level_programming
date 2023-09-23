65d3bb365241f0fbe25f11c4a644f0260493234d
Bawa232 <jbawa032@gmail.com>
2023-09-23 16:01:06 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa.
+Script that lists all State objects that contain the letter a
 """
 import sys
 from model_state import Base, State
@@ -9,22 +9,15 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    # check command line arguments
-    if len(sys.argv) != 5:
-        print(f"Usage: {sys.argv[0]} <mysql username>"
-                " <mysql password> <database name> <statename<>",
-            pool_pre_ping=True)
-        sys.exit(1)
-
     # connect to database using SQLAlchemy
-    username, password, db_name, state_name = sys.argv[1:]
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    usern, passw, dbn, state_name = sys.argv[1:]
+    engine = create_engine(f'mysql://{usern}:{passw}@localhost:3306/{dbn}')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    db_session = Session()
+    db_s = Session()
 
     # query for states that contain letter "a" and print them
-    state = db_session.query(State).filter_by(name=state_name).first()
+    state = db_s.query(State).filter_by(name=state_name).first()
     if not state:
         print("Not found")
     else:
